9ecf0a97496e42f9d9014ad16f7818f6273dba42
DO <tamazinatamazina@mail.ru>
2019-03-20 00:01:02 +0300

    Clicker

--- app/src/main/java/com/example/stargit2/MainActivity.java
+++ app/src/main/java/com/example/stargit2/MainActivity.java
@@ -2,17 +2,30 @@ package com.example.stargit2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        View.OnClickListener {
 
-    Button btn;
+    TextView mainTextView;
+    Button mainButton;
+    Integer clicked = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainTextView = findViewById(R.id.main_textview);
+        mainButton = findViewById(R.id.main_button);
+        mainButton.setOnClickListener(this);
 
-        btn = findViewById(R.id.button);
+    }
+
+    @Override
+    public void onClick(View v) {
+        clicked +=1;
+        mainTextView.setText(clicked.toString());
     }
 }
